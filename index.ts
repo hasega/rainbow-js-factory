264dbdde498866813cf27826e5d94038c667ea17
Haroldo Adans Ladeia Asega <hasega@Mac-mini-de-Haroldo.local>
2018-03-09 11:27:00 -0300

    mocha

--- index.ts
+++ index.ts
@@ -1,56 +1,47 @@
 'use strict';
 
-import {Observable} from 'rxjs';
-import {RxHttpRequest} from 'rx-http-request';
-
-/**
- * Adds commas to a number
- * @param {number} number
- * @param {string} locale
- * @return {string}
- */
+import { Observable } from 'rxjs';
+import { RxHttpRequest } from 'rx-http-request';
 
 
 
-var self = module.exports = {
-    numFormatter: function (number, locale) {
-        return number.toLocaleString(locale);
-    },
-    path:"",
-    init(p){this.path = p;},
-    getModules(): Observable < any > {
-        return RxHttpRequest.get(this.path);
-    },
-    restCall: function () {
-        var args = {
-            data: {
-                test: "hello"
-            }, // data passed to REST method (only useful in POST, PUT or PATCH methods)
-            path: {
-                "id": 120
-            }, // path substitution var
-            parameters: {
-                arg1: "hello",
-                arg2: "world"
-            }, // this is serialized as URL parameters
-            headers: {
-                "test-header": "client-api"
-            }, // request headers ,
-            opts: {
-                method: "VIEW"
-            }
-        };
 
-        console.log(this.path);
-    }
-};
-self.init("http://localhost:8081/");
-self
-    .getModules()
-    .subscribe((data) => {
+export function rainbowJSFactory()
+{
+    return {
+        numFormatter: function (number, locale) {
+            return number.toLocaleString(locale);
+        },
+        path: "",
+        init(p) { this.path = p; },
+        getModules(): Observable<any> {
+            return RxHttpRequest.get(this.path);
+        },
+        restCall: function () {
+            var args = {
+                data: {
+                    test: "hello"
+                }, // data passed to REST method (only useful in POST, PUT or PATCH methods)
+                path: {
+                    "id": 120
+                }, // path substitution var
+                parameters: {
+                    arg1: "hello",
+                    arg2: "world"
+                }, // this is serialized as URL parameters
+                headers: {
+                    "test-header": "client-api"
+                }, // request headers ,
+                opts: {
+                    method: "VIEW"
+                }
+            };
 
-        if (data.response.statusCode === 200) {
-            console.log(data.body); // Show the JSON response object.
+            console.log(this.path);
         }
-    }, (err) => console.error(err) // Show error in console
-    );
+    }
+}
+
+export default rainbowJSFactory;
+
+
---
+++ test/test.ts
@@ -0,0 +1,20 @@
+import rainbowJSFactory from '../index';
+import { expect } from 'chai';
+// if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
+// import 'mocha';
+describe('Hello function', () => {
+  it('should return hello world', () => {
+    const r = rainbowJSFactory();
+    r.init("http://localhost:8081/");
+    r.getModules()
+    .subscribe((data) => {
+
+        if (data.response.statusCode === 200) {
+            console.log(data.body); // Show the JSON response object.
+        }
+    }, (err) => console.error(err) // Show error in console
+    );
+
+    expect('Ok').to.equal('Ok');
+  });
+});
